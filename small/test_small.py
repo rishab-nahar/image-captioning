89570ab7f5d6a7cf5e8ea2ca4fd741243f4fe475
rishab-nahar <63296530+rishab-nahar@users.noreply.github.com>
2020-08-03 21:16:15 +0530

    Update test_small.py

--- small/test_small.py
+++ small/test_small.py
@@ -1,3 +1,5 @@
+
+
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.applications.xception import Xception
@@ -16,29 +18,28 @@ def load_doc(filename):
     return text
 
 
+def extract_features(fname, model):
+    try:
+        img = Image.open(fname)
 
-def extract_features(filename, model):
-        try:
-            image = Image.open(filename)
-            
-        except:
-            print("ERROR: Couldn't open image")
-        image = image.resize((299,299))
-        image = np.array(image)
-        # for images that has 4 channels, we convert them into 3 channels
-        if image.shape[2] == 4: 
-            image = image[..., :3]
-        image = np.expand_dims(image, axis=0)
-        image = image/127.5
-        image = image - 1.0
-        feature = model.predict(image)
-        return feature
+    except:
+        print("ERROR: Couldn't open image")
+    img = img.resize((299, 299))
+    img = np.array(img)
+    if img.shape[2] == 4:
+        img = img[..., :3]
+    img = np.expand_dims(img, axis=0)
+    img = img / 127.5
+    img = img - 1.0
+    feature = model.predict(img)
+    return feature
 
-def word_for_id(integer, tokenizer):
- for word, index in tokenizer.word_index.items():
-     if index == integer:
-         return word
- return None
+
+def word_for_id(n, tokenizer):
+    for word, ind in tokenizer.word_index.items():
+        if ind == n:
+            return word
+    return None
 
 
 def generate_desc(model, tokenizer, photo, max_length):
@@ -46,7 +47,7 @@ def generate_desc(model, tokenizer, photo, max_length):
     for i in range(max_length):
         sequence = tokenizer.texts_to_sequences([in_text])[0]
         sequence = pad_sequences([sequence], maxlen=max_length)
-        pred = model.predict([photo,sequence], verbose=0)
+        pred = model.predict([photo, sequence], verbose=0)
         pred = np.argmax(pred)
         word = word_for_id(pred, tokenizer)
         if word is None:
@@ -57,35 +58,32 @@ def generate_desc(model, tokenizer, photo, max_length):
     return in_text
 
 
-
 max_length = 32
-tokenizer = load(open("tokenizer.p","rb"))
+tokenizer = load(open("tokenizer.p", "rb"))
 model = load_model('models/model_25.h5')
 xception_model = Xception(include_top=False, pooling="avg")
 
 
 def load_dev(filename):
-    file=load_doc(filename)
-    dev_photos=file.split("\n")[:-1]
+    file = load_doc(filename)
+    dev_photos = file.split("\n")[:-1]
     return dev_photos
 
-dec_photos=load_dev("small_text/Flickr_8k.trainImages.txt")
+
+dec_photos = load_dev("small_text/Flickr_8k.trainImages.txt")
 
 import re
 
-
-
-k=0
-for i in range(k,k+10):
-
-    img_path="small_dataset/photos"+"/"+dec_photos[i]
+k = 0
+for i in range(k, k + 10):
+    img_path = "small_dataset/photos" + "/" + dec_photos[i]
     photo = extract_features(img_path, xception_model)
     img = Image.open(img_path)
 
     description = generate_desc(model, tokenizer, photo, max_length)
     print("\n\n")
-    description=re.sub(r"(red|pink|two|together)","",description)
-    description=re.sub(r"ocean","water",description)
+    description = re.sub(r"(red|pink|two|together)", "", description)
+    description = re.sub(r"ocean", "water", description)
     print(description)
-    
+
     plt.imshow(img)
